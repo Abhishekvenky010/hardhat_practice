593a6ff383579cdba78f0d963dff68bbb4cecbdc
Abhishekvenky010 <abhishekvenky010@gmail.com>
2026-06-25 11:25:32 +0530

    day2 left

---
+++ contracts/MyTokenV2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MyTokenV2 is ERC20,Ownable{
+    constructor ()ERC20("OpenZeppelin Token", "OZT") Ownable(msg.sender){
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+    function mint(
+    address to,
+    uint256 amount
+)
+    external
+    onlyOwner
+{
+    _mint(to, amount);
+}
+function burn(
+
+    uint256 amount
+)
+    external
+    onlyOwner
+{
+    _burn( msg.sender,amount);
+}
+}
